0c64fe79d47a85d255fb011bdc9c5a37b8c55d66
Mufan Qiu <mufan@cs.unc.edu>
2025-04-01 03:59:38 -0400

    update pretrain becnhmark script

--- scripts/eval/0326_benchmark_pretrain.sh
+++ scripts/eval/0326_benchmark_pretrain.sh
@@ -4,20 +4,27 @@
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3:1
 #SBATCH -t 1-00:05
 #SBATCH --mem=800G
-#SBATCH -o logs/slurm_benchmark/%j_%A_%a.out
-#SBATCH -e logs/slurm_benchmark/%j_%A_%a.err
+#SBATCH -o logs/slurm_benchmark_pretrain/%j_%A_%a.out
+#SBATCH -e logs/slurm_benchmark_pretrain/%j_%A_%a.err
 #SBATCH --mail-user=mufan@cs.unc.edu
 #SBATCH --mail-type=FAIL
-#SBATCH --array=0-0
-set -e
+#SBATCH --array=0-7%4
+set -ex
 
-cd /n/home08/zkong/mufan/tmp/moebench/open-instruct/
+cd /n/home08/zkong/mufan/tmp/moebench/OLMo
 [ -z "$SLURM_ARRAY_TASK_ID" ] && SLURM_ARRAY_TASK_ID=0
 
 export CUDA_VISIBLE_DEVICES=0
 
 model_dirs=(
-
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-1of8-sharedTrue/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-3of16-sharedTrue/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-7of32-sharedTrue/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-15of64-sharedTrue/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-2of8-sharedFalse/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-4of16-sharedFalse/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-8of32-sharedFalse/latest
+    /n/home08/zkong/mufan/tmp/moebench/OLMo/runs/0312-OLMoE-300M-15ep-16of64-sharedFalse/latest
 )
 
 mkdir -p results/baseline
@@ -52,12 +59,12 @@ CUDA_VISIBLE_DEVICES=0 lm_eval --model hf \
     --log_samples \
     --device cuda:0
 
-test_datasets=(
-    "scripts/eval/evaluation/I-CoNa.json"
-    "scripts/eval/evaluation/I-Controversial.json"
-    "scripts/eval/evaluation/I-MaliciousInstructions.json"
-)
-model_path="${model_dir}"
-for data_path in "${test_datasets[@]}"; do
-    python scripts/eval/0305_safety_benchmark.py --model_path $model_path --data_path $data_path
-done
+# test_datasets=(
+#     "scripts/eval/evaluation/I-CoNa.json"
+#     "scripts/eval/evaluation/I-Controversial.json"
+#     "scripts/eval/evaluation/I-MaliciousInstructions.json"
+# )
+# model_path="${model_dir}"
+# for data_path in "${test_datasets[@]}"; do
+#     python scripts/eval/0305_safety_benchmark.py --model_path $model_path --data_path $data_path
+# done

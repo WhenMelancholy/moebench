c61ea47d05022cb0e14e23be9f5612094857f9c2
Mufan Qiu <mufan@cs.unc.edu>
2025-06-10 01:41:16 -0700

    update readme

--- scripts/0226_download_lima.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download --repo-type dataset --local-dir ./lima GAIR/lima
-# HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download --repo-type dataset --local-dir ./tulu-v3.1-mix-preview-4096-OLMoE allenai/tulu-v3.1-mix-preview-4096-OLMoE
-HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download --repo-type model --local-dir ./OLMoE-1B-7B-0924 allenai/OLMoE-1B-7B-0924
--- scripts/eval/0328_convert_checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cd ~/mufan/tmp/moebench/open-instruct/output/0319_key_olmo7b/step_5000
-python zero_to_fp32.py . .
